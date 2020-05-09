e3b17b6126a53cb5320d520529194f369a0b27fa
wanghuan <2578145258@qq.com>
2020-05-09 22:17:44 +0800

    搭建demo

---
+++ mymall-security/src/main/java/com/jeason/mymallsecurity/MymallSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jeason.mymallsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymallSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallSecurityApplication.class, args);
+    }
+
+}
