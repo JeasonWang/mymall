14e7a97ada35a48d3e5c55d9307f4b8ff68be256
wanghuan <2578145258@qq.com>
2020-07-05 14:25:15 +0800

    初步搭建

---
+++ mymall-gateway/src/main/java/com/jeason/mymallgateway/MymallGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jeason.mymallgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallGatewayApplication.class, args);
+    }
+
+}
